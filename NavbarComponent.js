afd743bcee29dfeacf4c8f9efdf316932a6e5f15
Lexi Han <hanlexi@gmail.com>
2025-05-16 15:35:48 -0400

    Refactored Directory

--- NavbarComponent.js
+++
@@ -1,9 +0,0 @@
-const Navbar= () => (
-    <div class="navbar-container">
-      <ul class="navbar">
-        <li><a href="index.html">Home</a></li>
-        <li><a href="about.html">About</a></li>
-        <li><a href="projects.html">Projects</a></li>
-      </ul>
-    </div>
-)
---
+++ components/NavbarComponent.js
@@ -0,0 +1,9 @@
+const Navbar= () => (
+    <div class="navbar-container">
+      <ul class="navbar">
+        <li><a href="../pages/index.html">Home</a></li>
+        <li><a href="../pages/about.html">About</a></li>
+        <li><a href="../pages/projects.html">Projects</a></li>
+      </ul>
+    </div>
+)
